3ccf29f92ff9da633f17a4083f7493a5eb5c007d
stiefen1 <stephen-monnet@hotmail.com>
2023-11-07 16:04:31 +0100

    Remove previous modifications

--- robot_utils/pandaWrapper.hpp
+++ robot_utils/pandaWrapper.hpp
@@ -35,12 +35,12 @@ class PandaWrapper : public ArmRobotWrapper {
     //std::string get_ee_link_name(){ return "panda_tool"; } override
 
     // Limits from https://frankaemika.github.io/docs/control_parameters.html
-    Eigen::Matrix<double, NDOF, 1> min_position = {-2.8973, -1.7628, -2.8973, -3.0718, -2.8973, -0.0175, -2.8973};
-    Eigen::Matrix<double, NDOF, 1> max_position = {2.8973, 1.7628, 2.8973, -0.0698, 2.8973, 3.7525, 2.8973};
-    Eigen::Matrix<double, NDOF, 1> max_velocity = {2.1750, 2.1750, 2.1750, 2.1750, 2.6100, 2.6100, 2.6100};
-    Eigen::Matrix<double, NDOF, 1> max_acceleration = {15.0, 7.5, 10.0, 12.5, 15.0, 20.0, 20.0};
-    Eigen::Matrix<double, NDOF, 1> max_jerk = {7500, 3750, 5000, 6250, 7500, 10000, 10000};
-    Eigen::Matrix<double, NDOF, 1> max_torque = {87, 87, 87, 87, 12, 12, 12};
+    Eigen::Matrix<double, NDOF, 1> min_position {-2.8973, -1.7628, -2.8973, -3.0718, -2.8973, -0.0175, -2.8973};
+    Eigen::Matrix<double, NDOF, 1> max_position {2.8973, 1.7628, 2.8973, -0.0698, 2.8973, 3.7525, 2.8973};
+    Eigen::Matrix<double, NDOF, 1> max_velocity {2.1750, 2.1750, 2.1750, 2.1750, 2.6100, 2.6100, 2.6100};
+    Eigen::Matrix<double, NDOF, 1> max_acceleration {15.0, 7.5, 10.0, 12.5, 15.0, 20.0, 20.0};
+    Eigen::Matrix<double, NDOF, 1> max_jerk {7500, 3750, 5000, 6250, 7500, 10000, 10000};
+    Eigen::Matrix<double, NDOF, 1> max_torque {87, 87, 87, 87, 12, 12, 12};
     double max_torqueDot {1000};
 
     double max_linear_velocity {1.7};
